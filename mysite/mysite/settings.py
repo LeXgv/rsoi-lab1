095c4f178a9a25d8c7bcd6cc870f8cf8a5514406
Aleksey <lexgv@yandex.ru>
2019-09-09 11:53:52 +0300

    add test and deploy on heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -138,3 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if 'HEROKU_PLATFORM' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    django_heroku.settings(locals())
