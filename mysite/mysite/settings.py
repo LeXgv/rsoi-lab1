74a300502fed144b7c781066c588e2fb048ab81d
Aleksey <lexgv@yandex.ru>
2019-09-09 11:18:36 +0300

    add test and deploy on heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -85,6 +85,8 @@ if 'TRAVIS' in os.environ:
             'PORT': '5432',
         }
     }
+elif 'HEROKU_PLATFORM' in os.environ:
+    DATABASES = {'default': dj_database_url.config()}
 else:
     DATABASES = {
         'default': {
