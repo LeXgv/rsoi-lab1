e6b5d63466fb4c0b65cf7f391cef1fe4876e0872
Aleksey <lexgv@yandex.ru>
2019-09-09 12:48:30 +0300

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.api.apps.ApiConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,6 +13,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
